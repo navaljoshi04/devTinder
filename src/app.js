9fa1c7e6125dbd84b66374c0f77c96aba0c4a53b
navaljoshi04 <naval.joshi512@gmail.com>
2025-06-17 16:52:42 +0530

    sending the dynamic data from the API for DevTinder project

--- src/app.js
+++ src/app.js
@@ -4,18 +4,23 @@ import User from "./models/user.js";
 const app = express();
 connectWithDataBase();
 
+//middleware without this req.body will give undefined. 
+app.use(express.json());
 app.listen(3000, () => {
   console.log("server is succesfully listening on port 3000.....");
 });
 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "virat",
-    lastName: "kohli",
-    email: "viratkohli@gmail.com",
-    password: "viratkohli",
-  });
-
+    console.log(req.body);
+    //?this was the time when we were hardcoding the values : 
+//   const user = new User({
+//     firstName: "virat",
+//     lastName: "kohli",
+//     email: "viratkohli@gmail.com",
+//     password: "viratkohli",
+//   });
+ //? this is the dynamic way to save data from the api 
+  const user= new User(req.body);
   try {
     await user.save();
     res.send("user added successfully .....");
